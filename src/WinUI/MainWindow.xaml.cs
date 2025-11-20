6f436630d9a8ac0002614d4e4dd43a2507c3da22
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2025-11-20 22:45:52 +0900

    feat: MainWindow にコードビハインドのカウンター機能を追加します

--- src/WinUI/MainWindow.xaml.cs
+++ src/WinUI/MainWindow.xaml.cs
@@ -1,31 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace ABCDEApplicationTemplate
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// カウンター付きのメインウィンドウです。
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private int _count = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            UpdateCounterText();
+        }
+
+        private void IncrementButton_Click(object sender, RoutedEventArgs e)
+        {
+            _count++;
+            UpdateCounterText();
+        }
+
+        private void UpdateCounterText()
+        {
+            CounterTextBlock.Text = _count.ToString();
         }
     }
 }
