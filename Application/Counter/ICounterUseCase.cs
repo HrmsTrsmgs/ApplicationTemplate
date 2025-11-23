8d854c82858d941961ff8d792b500f2ace6aeac0
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2025-11-23 21:40:54 +0900

    feat: Counter 用の UseCase インターフェースを追加します

---
+++ Application/Counter/ICounterUseCase.cs
@@ -0,0 +1,7 @@
+﻿namespace Marimo.ABCDEApplicationTemplate.Application.Counter;
+
+public interface ICounterUseCase
+{
+    Task<int> GetAsync(CancellationToken cancellationToken = default);
+    Task<int> IncrementAsync(CancellationToken cancellationToken = default);
+}
